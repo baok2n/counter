17e1ac9bbbae8b288f846f3628e2da84eb43dc97
baro tran <bao.tran@terralogic.com>
2019-06-23 21:02:32 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [db, setDb] = useState(74);
+  const [resist, setResist] = useState(13);
+  const [miss, setMiss] = useState(29);
+
+  const eventHandler = e => {
+    switch (e.keyCode) {
+      case 65:
+        console.log('get here');
+        setDb(db + 1);
+        break;
+      case 83:
+        setResist(resist + 1);
+        break;
+      case 68:
+        setMiss(miss + 1);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={eventHandler} tabIndex="0" >
+      <p>{`[A] def breaks: ${db}`}</p>
+      <p>{`[S] resistances: ${resist}`}</p>
+      <p>{`[D] misses(no defbreak no resist): ${miss}`}</p>
+      <p>{`total hits: ${db + resist + miss}`}</p>
     </div>
   );
 }
